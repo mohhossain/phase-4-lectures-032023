28cd7a433f13d773c794433f8efcc909544f013e
Mohammad <moh.hossain98@gmail.com>
2023-05-24 11:35:14 -0400

    starter

--- 03-REST-API-Flask-pt2/server/models.py
+++ 03-REST-API-Flask-pt2/server/models.py
@@ -1,12 +1,8 @@
-
 from flask_sqlalchemy import SQLAlchemy
-# 6. ✅ Import `SerializerMixin` from `sqlalchemy_serializer`
-
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-# 7. ✅ Pass `SerializerMixin` to `Productions`
 class Production(db.Model, SerializerMixin):
     __tablename__ = 'productions'
 
@@ -20,19 +16,14 @@ class Production(db.Model, SerializerMixin):
     ongoing = db.Column(db.Boolean)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    crew_members = db.relationship('CrewMember', backref='production')
+        
+    serialize_rules = ('-crew_members.production',)
 
-    # multiple crew members
-    crew_members = db.relationship('CrewMember', backref = 'production')
-
-    # 7.1 ✅ Create a serialize rule that will help add our `crew_members` to the response.
-    serialize_only = ('id', 'title', 'director', 'ongoing', 'crew_members')
-    serialize_rules = ('-crew_members.production',) 
-    
 
     def __repr__(self):
         return f'<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>'
 
-# 8. ✅ Pass `SerializerMixin` to `CrewMember`
 class CrewMember(db.Model, SerializerMixin):
     __tablename__ = 'crew_members'
 
@@ -41,15 +32,10 @@ class CrewMember(db.Model, SerializerMixin):
     role = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    
-    # foreign key
     production_id = db.Column(db.Integer, db.ForeignKey('productions.id'))
-    # 8.1 ✅ Create a serialize rule that will help add our `production` to the response.
-      
+    
+    serialize_rules = ('-production.crew_members',)
+
     def __repr__(self):
         return f'<Production Name:{self.name}, Role:{self.role}'
-    
-    serialize_only = ('id', 'name', 'role')
-    # serialize_rules = ('-production.crew_members',) 
 
- # 9. ✅ Navigate back to `app.py` for further steps.
